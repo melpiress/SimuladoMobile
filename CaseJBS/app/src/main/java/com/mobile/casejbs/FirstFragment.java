d3f62999e8e5bb45eb8fc62dffdeef577c391c5a
laurawada <laura.wada@germinare.org.br>
2025-06-11 08:33:34 -0300

    Corrigindo first fragment

--- CaseJBS/app/src/main/java/com/mobile/casejbs/FirstFragment.java
+++ CaseJBS/app/src/main/java/com/mobile/casejbs/FirstFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.mobile.casejbs.databinding.FragmentFirstBinding;
+
+import java.time.LocalDateTime;
 
 public class FirstFragment extends Fragment {
 
@@ -26,7 +31,23 @@ public class FirstFragment extends Fragment {
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.enviaPlaca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText placa = new EditText(getContext());
+                Veiculo novoVeiculo = new Veiculo(placa.getText().toString(), LocalDateTime.now());
 
+                NavHostFragment navHost = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.FirstFragment);
+                Fragment fragAtual = navHost.getChildFragmentManager().getPrimaryNavigationFragment();
+                if (fragAtual instanceof FirstFragment) {
+                    ((SecondFragment) fragAtual).adicionarVeiculo(novoVeiculo);
+                }
+                Toast.makeText(getContext(), "Veículo adicionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public void onDestroyView() {
